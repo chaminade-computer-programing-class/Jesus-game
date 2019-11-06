ebf1118cfa8dd25d6e7282290ae77712626afb2d
McD Cook <42852932+BandTurtle@users.noreply.github.com>
2019-11-06 11:07:37 -0800

    Add files via upload

---
+++ game1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class game1{
+    public static void main(String[] args) throws InterruptedException {
+    Scanner scan = new Scanner(System.in);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+}
